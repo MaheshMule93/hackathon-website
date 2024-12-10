222c090805d9fd3eca6a782213399e56713ef796
MaheshMule93 <40827338+MaheshMule93@users.noreply.github.com>
2024-12-10 19:08:41 +0530

    Added website

---
+++ hackathon_website/src/app/layout.js
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+import "./globals.css";
+import NavBar from "./components/Navbar/Navbar";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Hackathon Website",
+  description: "Website to try out Performance",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <NavBar/>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-orange-50">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            {children}
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
